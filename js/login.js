ee318be87ffdd5f2b83d32d352d010ef4c638405
prasanna-adhikari <prasannaadhikari022@gmail.com>
2020-03-26 16:55:00 +0545

    Login frontend

---
+++ js/login.js
@@ -0,0 +1,34 @@
+(function($) {
+    "use strict";
+    $('.input').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+
+    var input = $('.validate-input .input');
+
+        $('.validate-form').on('submit',function(){
+            var check = true;
+
+            for(var i=0; i<input.length; i++) {
+                if(validate(input[i]) == false){
+                    showValidate(input[i]);
+                    check=false;
+                }
+            }
+
+            return check;
+    });
+    $('.validate-form .input').each(function(){
+        $(this).focus(function(){
+        hideValidate(this);
+        });
+    });
+});
