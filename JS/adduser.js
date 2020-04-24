475598e2440ff886428225f22e2add4512237198
prasanna-adhikari <prasannaadhikari022@gmail.com>
2020-04-24 22:01:46 +0545

    Add user form added in admin user page

---
+++ JS/adduser.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("addUserForm").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("addUserForm").style.display="none";
+  }
