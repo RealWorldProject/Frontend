a75dd654865408f74880c965cff35b874ecefd63
nibritipoudel <nibriti.poudel1@gmail.com>
2020-04-26 16:43:06 +0545

    Added item page

---
+++ JS/additem.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("addItemForm").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("addItemForm").style.display="none";
+  }
